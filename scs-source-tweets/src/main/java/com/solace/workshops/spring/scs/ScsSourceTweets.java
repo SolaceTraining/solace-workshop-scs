71e41c5239c123fb27c58eb775f56b3be5d47de3
Marc DiPasquale <Marc.DiPasquale@solace.com>
2019-03-29 14:05:26 -0400

    Implement Unit Tests, Add Comments, Add License Header

--- scs-source-tweets/src/main/java/com/solace/workshops/spring/scs/ScsSourceTweets.java
+++ scs-source-tweets/src/main/java/com/solace/workshops/spring/scs/ScsSourceTweets.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.solace.workshops.spring.scs;
 
 import java.io.BufferedReader;
@@ -47,26 +66,24 @@ public class ScsSourceTweets {
 		SpringApplication.run(ScsSourceTweets.class, args);
 	}
 
-	// Method that sends a tweet every fixedRate ms
+	// Define an output to send to; every fixedRate ms
 	@InboundChannelAdapter(channel = Source.OUTPUT, poller = @Poller(fixedRate = "1000"))
-//	public Message<?> sendTweet() {
 	public Tweet sendTweet() {
 		Tweet nextTweet;
 		if (tweetIterator.hasNext()) {
 			nextTweet = tweetIterator.next();
 		} else {
-			// No more tweet templates so start over
+			// No more tweets left in our list; start over from beginning
 			tweetIterator = tweetList.iterator();
 			nextTweet = tweetIterator.next();
 		}
 		nextTweet.updateTimestamp();
 		nextTweet.updateUuid();
-		System.out.println(nextTweet.toString());
 		log.info(nextTweet.toString());
 		return nextTweet;
 	}
 
-	// Method to read the file of canned tweets
+	// Create our list of tweets from the file 'filename'
 	private ArrayList<Tweet> readFile(String filename) {
 		ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 		try {
