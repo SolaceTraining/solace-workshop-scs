65444853d98d006cb2aca17d0965dcabe89bc23a
Marc DiPasquale <Marc.DiPasquale@solace.com>
2019-08-28 15:38:12 -0400

    Update to remove PCF references

--- 10-spring-boot-mqttwebapp/src/main/java/com/solace/workshops/spring/scs/MqttWebApp.java
+++ 10-spring-boot-mqttwebapp/src/main/java/com/solace/workshops/spring/scs/MqttWebApp.java
@@ -1,5 +1,6 @@
 package com.solace.workshops.spring.scs;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MqttWebApp {
 
+	static org.slf4j.Logger logger = LoggerFactory.getLogger(MqttWebApp.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MqttWebApp.class, args);
+		logger.info("Navigate to http://localhost:8090/mqttListener.html to see incoming tweets!");
 	}
 
 	@RequestMapping("/")
 	String home() {
+		logger.info("Someone made a request to '/'");
 		return "Welcome to the Solace Spring Cloud Streams Workshop! "
-				+ "Lookup your route in Apps Manager or via CLI:'cf app spring-boot-mqttwebapp' "
-				+ "Navigate to https://<INSERT ROUTE>/mqttListener.html to see incoming tweets!";
+				+ "Navigate to http://localhost:8090/mqttListener.html to see incoming tweets!";
 	}
 
 }
